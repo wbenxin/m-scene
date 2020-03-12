b1f18adbb8c485fda136f47ce6ee22d355c0582e
wangbenxin <benxin@126.com>
2020-03-13 00:03:34 +0800

    增加对企业微信的支持

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 const Wechat = require('./lib/wechat');
+const Wework = require('./lib/wework');
+
+const token_dir = path.join(__dirname, '.cache/access_tokens');
+fs.mkdir(token_dir, { recursive: true });
+// 每分钟检查一次, 超过2小时未更新的直接删掉
+setInterval(async () => {
+  let files = await fs.readdir(token_dir);
+  files.forEach(async file => {
+    let stats = await fs.stat(path.join(token_dir, file));
+    if (Date.now() - stats.mtimeMs > 7200 * 1000) {
+      await fs.unlink(path.join(token_dir, file));
+    }
+  });
+}, 60 * 1000);
 
 exports.Wechat = Wechat;
+exports.Wework = Wework;
